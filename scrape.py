d1a349ac5e35275b52401f151593f577a03113e9
John Philip <john.philip@buzzfeed.com>
2021-01-03 17:17:03 -0500

    Remove unnecessary headers, and clean up arg parsing

--- scrape.py
+++ scrape.py
@@ -8,8 +8,8 @@ from io import BytesIO
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-p', '-playlist', type=str, default="playlist.m3u8", help='Playlist file in the .m3u8 format.')
-parser.add_argument('-f', '-final_track_name', type=str, default="MADEON_SET.mp3", help='Final track name.')
+parser.add_argument('-p', '-playlist', required=True, type=str, help='Playlist file in the .m3u8 format.')
+parser.add_argument('-f', '-final_track_name', required=True, type=str, help='Final track name.')
 
 args = parser.parse_args()
 
@@ -18,34 +18,12 @@ final_track_name = args.f
 
 if __name__ == '__main__':
 
-  # headers = {
-  #   "Accept": "*/*",
-  #   "Accept-Encoding": "gzip, deflate, br",
-  #   "Accept-Language": "en-US,en;q=0.9",
-  #   "Cache-Control": "no-cache",
-  #   "Connection": "keep-alive",
-  #   "Host": "cf-hls-media.sndcdn.com",
-  #   "Origin": "https://soundcloud.com",
-  #   "Pragma": "no-cache",
-  #   "Referer": "https://soundcloud.com/",
-  #   "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-  #   "sec-ch-ua-mobile": "?0",
-  #   "Sec-Fetch-Dest": "empty",
-  #   "Sec-Fetch-Mode": "cors",
-  #   "Sec-Fetch-Site": "cross-site",
-  #   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
-  # }
-
   final_track = None
   count = 1
   with open(playlist_file, 'r') as playlist:
     for line in playlist:
       if line.startswith('https://'):
-        res = None
-        try:
-          res = requests.get(line.strip()) 
-        except Exception as e:
-          print("SOMETHING FAILED LMAO")
+        res = requests.get(line.strip())
         print(f"Downloading snippet {count}...")
         if res.ok:
           snippet = AudioSegment.from_mp3(BytesIO(res.content))
