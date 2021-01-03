8461ff935c4aab16079ff7bdfde4c63b5d549e28
John Philip <john.philip@buzzfeed.com>
2021-01-03 16:59:51 -0500

    get rid of intermediate file writing

--- scrape.py
+++ scrape.py
@@ -8,47 +8,44 @@ from io import BytesIO
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-o', '-output', type=str, default="MADEON", help='Output folder for mp3 snippets.')
 parser.add_argument('-p', '-playlist', type=str, default="playlist.m3u8", help='Playlist file in the .m3u8 format.')
 parser.add_argument('-f', '-final_track_name', type=str, default="MADEON_SET.mp3", help='Final track name.')
 
 args = parser.parse_args()
 
 playlist_file = args.p
-folder_name = args.o
 final_track_name = args.f
 
 if __name__ == '__main__':
-  
-  folder = os.path.join(os.getcwd(), folder_name)
 
-  if not os.path.exists(folder):
-    os.mkdir(folder)
-
-  headers = {
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Cache-Control": "no-cache",
-    "Connection": "keep-alive",
-    "Host": "cf-hls-media.sndcdn.com",
-    "Origin": "https://soundcloud.com",
-    "Pragma": "no-cache",
-    "Referer": "https://soundcloud.com/",
-    "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-    "sec-ch-ua-mobile": "?0",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "cross-site",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
-  }
+  # headers = {
+  #   "Accept": "*/*",
+  #   "Accept-Encoding": "gzip, deflate, br",
+  #   "Accept-Language": "en-US,en;q=0.9",
+  #   "Cache-Control": "no-cache",
+  #   "Connection": "keep-alive",
+  #   "Host": "cf-hls-media.sndcdn.com",
+  #   "Origin": "https://soundcloud.com",
+  #   "Pragma": "no-cache",
+  #   "Referer": "https://soundcloud.com/",
+  #   "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
+  #   "sec-ch-ua-mobile": "?0",
+  #   "Sec-Fetch-Dest": "empty",
+  #   "Sec-Fetch-Mode": "cors",
+  #   "Sec-Fetch-Site": "cross-site",
+  #   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+  # }
 
   final_track = None
   count = 1
   with open(playlist_file, 'r') as playlist:
     for line in playlist:
       if line.startswith('https://'):
-        res = requests.get(line.strip(), headers=headers)
+        res = None
+        try:
+          res = requests.get(line.strip()) 
+        except Exception as e:
+          print("SOMETHING FAILED LMAO")
         print(f"Downloading snippet {count}...")
         if res.ok:
           snippet = AudioSegment.from_mp3(BytesIO(res.content))
@@ -58,4 +55,6 @@ if __name__ == '__main__':
             final_track = snippet
         count += 1
   
+  print("\nExporting final track...")
   final_track.export(final_track_name, format="mp3")
+  print("\nDone!")
